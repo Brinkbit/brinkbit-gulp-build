b93061b99d103bba079c7a88bfd55e5ac1ffe625
Justin Livi <justin@brinkbit.com>
2016-02-29 14:16:49 -0500

    Add configurations for es6, es5 and less
    Add API for compiling es6 without watching for changes. Refactor with
    minor efficiency enhancements. Add es5 and less build tasks.

--- test/index.js
+++ test/index.js
@@ -20,13 +20,13 @@ const resourcesExist = paths => Promise.all(
     R.reduce( R.and, true )( res )
 ));
 
-describe( '#buildJs', function() {
+describe( '#buildEs6', function() {
     describe( 'many to many', function() {
         const testFiles = [
-            'testFiles/multiple/outer.min.js',
-            'testFiles/multiple/outer.min.js.map',
-            'testFiles/multiple/deep/inner.min.js',
-            'testFiles/multiple/deep/inner.min.js.map',
+            'testFiles/es6/multiple/outer.min.js',
+            'testFiles/es6/multiple/outer.min.js.map',
+            'testFiles/es6/multiple/deep/inner.min.js',
+            'testFiles/es6/multiple/deep/inner.min.js.map',
         ];
 
         after( function() {
@@ -35,7 +35,7 @@ describe( '#buildJs', function() {
 
         it( 'should preserve file structure for globs', function() {
             this.timeout( 5000 );
-            return brinkbuild.buildJs( 'testFiles/multiple/**/*.js' )
+            return brinkbuild.buildEs6( 'testFiles/es6/multiple/**/*.js' )
             .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
         });
     });
@@ -43,33 +43,101 @@ describe( '#buildJs', function() {
     describe( 'many to one', function() {
         it( 'should concat multiple files into one', function() {
             const testFiles = [
-                'testFiles/single.min.js',
-                'testFiles/single.min.js.map',
+                'testFiles/es6/single.min.js',
+                'testFiles/es6/single.min.js.map',
             ];
 
             after( function() {
                 return Promise.all( testFiles.map( path => fs.removeAsync( path )));
             });
 
-            return brinkbuild.buildJs( 'testFiles/multiple/**/*.js', 'testFiles', 'single.min.js' )
+            return brinkbuild.buildEs6( 'testFiles/es6/multiple/**/*.js', 'testFiles/es6', 'single.min.js' )
             .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
         });
     });
 
     describe( 'watch and rebuild', function() {
         const testFiles = [
-            'testFiles/watch.min.js',
-            'testFiles/watch.min.js.map',
+            'testFiles/es6/watch.min.js',
+            'testFiles/es6/watch.min.js.map',
         ];
 
         before( function() {
-            return fs.copyAsync( 'testFiles/watch.js', 'testFiles/temp/watch.js', {});
+            return fs.copyAsync( 'testFiles/es6/watch.js', 'testFiles/temp/watch.js', {});
         });
 
         after( function() {
             return Promise.all(
                 testFiles.map( path => fs.removeAsync( path ))
-                .concat( fs.moveAsync( 'testFiles/temp/watch.js', 'testFiles/watch.js', { clobber: true })
+                .concat( fs.moveAsync( 'testFiles/temp/watch.js', 'testFiles/es6/watch.js', { clobber: true })
+                    .then(() => fs.removeAsync( 'testFiles/temp' ))
+                )
+            );
+        });
+
+        it( 'should not automatically rebuild files on changes', function() {
+            this.timeout( 3000 );
+            return brinkbuild.buildEs6( 'testFiles/es6/watch.js', 'testFiles/es6', 'watch.min.js' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true )
+            .then(() => Promise.all( testFiles.map( path => fs.removeAsync( path ))))
+            .then(() => new Promise( resolve => setTimeout( resolve, 1000 )))
+            .then(() => fs.appendFileAsync( 'testFiles/es6/watch.js', ' ', {}))
+            .then(() => new Promise( resolve => setTimeout( resolve, 1000 )))
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.false );
+        });
+    });
+});
+
+describe( '#watchEs6', function() {
+    describe( 'many to many', function() {
+        const testFiles = [
+            'testFiles/es6/multiple/outer.min.js',
+            'testFiles/es6/multiple/outer.min.js.map',
+            'testFiles/es6/multiple/deep/inner.min.js',
+            'testFiles/es6/multiple/deep/inner.min.js.map',
+        ];
+
+        after( function() {
+            return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+        });
+
+        it( 'should preserve file structure for globs', function() {
+            this.timeout( 5000 );
+            return brinkbuild.watchEs6( 'testFiles/es6/multiple/**/*.js' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+
+    describe( 'many to one', function() {
+        it( 'should concat multiple files into one', function() {
+            const testFiles = [
+                'testFiles/es6/single.min.js',
+                'testFiles/es6/single.min.js.map',
+            ];
+
+            after( function() {
+                return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+            });
+
+            return brinkbuild.watchEs6( 'testFiles/es6/multiple/**/*.js', 'testFiles/es6', 'single.min.js' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+
+    describe( 'watch and rebuild', function() {
+        const testFiles = [
+            'testFiles/es6/watch.min.js',
+            'testFiles/es6/watch.min.js.map',
+        ];
+
+        before( function() {
+            return fs.copyAsync( 'testFiles/es6/watch.js', 'testFiles/temp/watch.js', {});
+        });
+
+        after( function() {
+            return Promise.all(
+                testFiles.map( path => fs.removeAsync( path ))
+                .concat( fs.moveAsync( 'testFiles/temp/watch.js', 'testFiles/es6/watch.js', { clobber: true })
                     .then(() => fs.removeAsync( 'testFiles/temp' ))
                 )
             );
@@ -77,12 +145,83 @@ describe( '#buildJs', function() {
 
         it( 'should automatically rebuild files on changes', function() {
             this.timeout( 3000 );
-            return brinkbuild.buildJs( 'testFiles/watch.js', 'testFiles', 'watch.min.js' )
+            return brinkbuild.watchEs6( 'testFiles/es6/watch.js', 'testFiles/es6', 'watch.min.js' )
             .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true )
             .then(() => Promise.all( testFiles.map( path => fs.removeAsync( path ))))
             .then(() => new Promise( resolve => setTimeout( resolve, 1000 )))
-            .then(() => fs.appendFileAsync( 'testFiles/watch.js', ' ', {}))
+            .then(() => fs.appendFileAsync( 'testFiles/es6/watch.js', ' ', {}))
             .then(() => new Promise( resolve => setTimeout( resolve, 1000 )))
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+});
+
+describe( '#buildEs5', function() {
+    describe( 'many to many', function() {
+        const testFiles = [
+            'testFiles/es5/multiple/outer.min.js',
+            'testFiles/es5/multiple/outer.min.js.map',
+            'testFiles/es5/multiple/deep/inner.min.js',
+            'testFiles/es5/multiple/deep/inner.min.js.map',
+        ];
+
+        after( function() {
+            return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+        });
+
+        it( 'should preserve file structure for globs', function() {
+            this.timeout( 5000 );
+            return brinkbuild.buildEs5( 'testFiles/es5/multiple/**/*.js', 'testFiles/es5/multiple' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+
+    describe( 'many to one', function() {
+        it( 'should concat multiple files into one', function() {
+            const testFiles = [
+                'testFiles/es5/single.min.js',
+                'testFiles/es5/single.min.js.map',
+            ];
+
+            after( function() {
+                return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+            });
+
+            return brinkbuild.buildEs5( 'testFiles/es5/multiple/**/*.js', 'testFiles/es5', 'single.min.js' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+});
+
+describe( '#buildLess', function() {
+    describe( 'many to many', function() {
+        const testFiles = [
+            'testFiles/less/multiple/outer.min.css',
+            'testFiles/less/multiple/deep/inner.min.css',
+        ];
+
+        after( function() {
+            return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+        });
+
+        it( 'should preserve file structure for globs', function() {
+            this.timeout( 5000 );
+            return brinkbuild.buildLess( 'testFiles/less/multiple/**/*.less', 'testFiles/less/multiple' )
+            .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
+        });
+    });
+
+    describe( 'many to one', function() {
+        it( 'should concat multiple files into one', function() {
+            const testFiles = [
+                'testFiles/less/single.min.css',
+            ];
+
+            after( function() {
+                return Promise.all( testFiles.map( path => fs.removeAsync( path )));
+            });
+
+            return brinkbuild.buildLess( 'testFiles/less/multiple/**/*.less', 'testFiles/less', 'single.min.css' )
             .then(() => expect( resourcesExist( testFiles )).to.eventually.be.true );
         });
     });
